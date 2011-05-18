d26a0b99424c4d5c40f52f9adcd4624b17135cf3
Suraj N. Kurapati <sunaku@gmail.com>
2011-05-17 23:03:59 -0700

    ignore all changes in submodules in `git status`

--- submodule.sh
+++ submodule.sh
@@ -4,3 +4,8 @@ git submodule sync
 git submodule init
 git submodule status | awk '/^-/ { print $2 }' | xargs -r git submodule update
 git submodule foreach git pull origin master
+
+# ignore all changes in submodules in `git status`
+for submodule in $(git submodule status | awk '{print $2}'); do
+  git config "submodule.$submodule.ignore" all
+done
